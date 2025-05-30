bbcccd5a0f23010187252174873ddbbf29b98341
lgurita <louisguritapro@gmail.com>
2025-05-30 09:53:27 +0200

    keytab custom path

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,12 +1,14 @@
 module.exports = {
     apps: [
         {
-            name: 'spotly-app',
+            name: 'spotly',
             script: 'npm',
             args: 'start',
             env: {
-                NODE_ENV: 'production'
-            }
+                NODE_ENV: 'production',
+                KRB5_KTNAME: '/etc/apache2/fhm.keytab',
+            },
+            autorestart: true,
         },
         {
             name: 'spotly-cron',
