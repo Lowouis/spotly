1bfe22015b47daef45a24d04754534d496bdecf1
louisgurita <louisguritapro@gmail.com>
2025-08-22 10:04:47 +0200

    favicon

--- app/metadata.js
+++ app/metadata.js
@@ -1,4 +1,9 @@
 export const metadata = {
     title: "Spotly",
     description: "Booking your resources, our priority",
+    icons: {
+        icon: '/favicon.ico',
+        shortcut: '/favicon.ico',
+        apple: '/favicon.ico',
+    },
 };
